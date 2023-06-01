af92d8e51f0439e78d3d16b771ea47ee667318b4
regianeclara <87345295+regianeclara@users.noreply.github.com>
2023-05-31 22:23:55 -0300

    Add files via upload

---
+++ Node/ExerciciosAnteriores/exercicio5.js
@@ -0,0 +1,10 @@
+var eventos = require('events');
+var EmissorEventos = eventos.EventEmitter;
+var ee = new EmissorEventos();
+
+ee.on('dados',function(fecha){
+    console.log(fecha);
+});
+setInterval(function(){
+    ee.emit('dados',Date.now());
+},500);
